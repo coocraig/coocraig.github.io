6aa76c046674e078c6714f37880cdc67a0c2bbd2
CJCraig01 <cooperjcraig01@gmail.com>
2022-10-19 04:18:57 -0600

    g

--- sites/raster/main.js
+++ sites/raster/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $("input.slider").on("input change", function(event) {
         var pos = event.target.value;
 
-        $("div.before").css({width: pos + "%"});
+        $("div.before").css({width: pos + "px"});
         $("div.slider-button").css({left: "calc(" + pos + "% - 18px)"});
     });
 
